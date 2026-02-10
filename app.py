1fa29d06fc09f879ef8f2db20863ef10873b317c
Yayanimal <ynidiri@gmail.com>
2026-02-10 16:49:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ generation_config = {
 
 try:
     model = genai.GenerativeModel(
-        model_name="gemini-2.0-flash", 
+        model_name="gemini-1.5-flash", 
         generation_config=generation_config
     )
 except Exception as e:
